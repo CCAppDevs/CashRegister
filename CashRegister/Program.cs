bf87a5c2c21c669de3b014213a832ef4bc922503
Jesse Harlan <jesse.harlan@centralia.edu>
2025-10-20 10:06:29 -0700

    Add project files.

---
+++ CashRegister/Program.cs
@@ -0,0 +1,94 @@
+﻿namespace CashRegister
+{
+    // --- Verbs/Actions - Things we can do ---
+    // Menu
+    // Prompt the user
+    // Add to the list
+    // Calculate Function
+    // Change Tax Rate
+    // Print the list
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // App that calculates the total of many items and the subtotal after tax
+
+            // --- Nouns/Variables - Things we know ---
+            double[] prices; // TODO: figure out how many is reasonable
+            double taxRate = 0.10;
+            bool isRunning = true;
+            int choice = -1;
+
+            while (isRunning)
+            {
+                PrintMenu();
+                // capture the users choice
+                choice = PromptInt("What would you like to do?");
+
+                // choose which action to do based on value of choice
+                switch (choice)
+                {
+                    case 0:
+                        // exit
+                        isRunning = false;
+                        break;
+                    default:
+                        // invalid choice, something went wrong
+                        Console.WriteLine("Invalid choice, please try again");
+                        break;
+                }
+
+            }
+        }
+
+        static void PrintMenu()
+        {
+            Console.WriteLine("#######################");
+            Console.WriteLine("Main Menu");
+            Console.WriteLine("#######################");
+            Console.WriteLine();
+            Console.WriteLine("1. Add Item");
+            Console.WriteLine("2. Print List");
+            Console.WriteLine("3. Calculate Tax");
+            Console.WriteLine("4. Change Tax Rate");
+            Console.WriteLine("0. Exit");
+        }
+
+        static int PromptInt(string message)
+        {
+            Console.Write(message + " ");
+            int value = -1;
+
+            // try to parse the input to an integer and place in value
+            if (Int32.TryParse(Console.ReadLine(), out value))
+            {
+                // return the succesful parse
+                return value;
+            } else
+            {
+                // return an unsuccesful parse
+                return -1;
+            }
+        }
+
+        static void AddItem(double price)
+        {
+
+        }
+
+        static void Calculate()
+        {
+
+        }
+
+        static void ChangeTaxRate(double rate)
+        {
+
+        }
+
+        static void PrintList()
+        {
+
+        }
+    }
+}
